3d73e65e67f52baef6a9fda03fef6688a03974bf
UFO-010 <smeliy.kirill2014@yandex.ru>
2025-10-02 13:05:59 +0300

    Replaced switch with callbacks array

--- include/logger.h
+++ include/logger.h
@@ -83,7 +83,7 @@ public:
 class Logger {
 public:
     /**
-     * @brief Logger::setLogLevel
+     * @brief setLogLevel
      * @param level priority of messages to display
      * Set logging level. Messages with a lower priority level will be ignored
      */
@@ -92,7 +92,7 @@ public:
     static int getLevel() { return log_level; }
 
     /**
-     * @brief Logger::setLogPattern
+     * @brief setLogPattern
      * @param pattern Output message pattern
      * Define ouput logging message format to look like.
      * Options : "%{date}"; "%{time}"; "%{type}"; "%{file}"; "%{thread}";
@@ -172,6 +172,16 @@ public:
         user_handler = _handler;
     }
 
+    /**
+     * @brief log
+     * @param msgType type of message, see @brief messageType
+     * @param file name of file function called from
+     * @param func name of function this function called from
+     * @param line number of function this function called from
+     * @param str input string to print in log message
+     *
+     * Main logging function. Calls provided sinks and callbacks if enabled
+     */
     static void log(
         const messageType &msgType, const char *file, const char *func, int line, const char *str) {
         if (log_level < static_cast<int>(msgType)) {
@@ -194,8 +204,8 @@ public:
     }
 
     /**
-     * @brief Logger::createMessage
-     * @param msgType type of message, see @param messageType
+     * @brief createMessage
+     * @param msgType type of message, see @brief messageType
      * @param file name of file function called from
      * @param func name of function this function called from
      * @param line number of function this function called from
@@ -203,7 +213,7 @@ public:
      * @param outBuf poitner to output buffer
      * @param bufSize max size of buffer
      *
-     * Creates log message with specified in @brief Logger::setMessagePattern
+     * Creates log message with specified in @brief setMessagePattern
      * view and put it into `outBuf` and control it's size with `bufSize`.
      */
     static size_t createMessage(const messageType &msgType,
@@ -215,72 +225,12 @@ public:
                                 size_t bufSize) {
         size_t pos = 0;
 
-        auto append = [&pos, bufSize, outBuf](const char *data, size_t len) {
-            size_t cnt = 0;
-
-            if (pos + len < bufSize) {
-                cnt = len;
-            } else if (bufSize > pos) {
-                cnt = bufSize - pos - 1;
-            } else {
-                cnt = 0;
-            }
-            if (cnt != 0) {
-                std::memcpy(outBuf + pos, data, cnt);
-                pos += cnt;
-            }
-        };
-
-        auto appendC = [append](const char *s) { append(s, std::strlen(s)); };
-
-        auto appendInt = [append](int v) {
-            char numbuf[LOGGER_MAX_NUMBUF_SIZE];
-            int n = std::snprintf(numbuf, sizeof(numbuf), "%d", v);
-            if (n > 0) {
-                append(numbuf, static_cast<size_t>(n));
-            }
-        };
-
         char temp[LOGGER_MAX_TEMP_SIZE];
         for (size_t i = 0; i < token_ops_count; i++) {
-            append(tokenOps[i].literal, tokenOps[i].literal_len);
-            switch (tokenOps[i].type) {
-                case TokDate:
-                    data_provider->getCurrentDate(temp, sizeof(temp));
-                    appendC(temp);
-                    break;
-                case TokTime:
-                    data_provider->getCurrentTime(temp, sizeof(temp));
-                    appendC(temp);
-                    break;
-                case TokLevel:
-                    appendC(msg_log_types[static_cast<int>(msgType)]);
-                    break;
-                case TokFile:
-                    appendC(file);
-                    break;
-                case TokThread:
-                    data_provider->getThreadId(temp, sizeof(temp));
-                    appendC(temp);
-                    break;
-                case TokFunc:
-                    appendC(func);
-                    break;
-                case TokLine:
-                    appendInt(line);
-                    break;
-                case TokPid:
-                    data_provider->getProcessName(temp, sizeof(temp));
-                    appendC(temp);
-                    break;
-                case TokMessage:
-                    appendC(str);
-                    break;
-                case TokInvalid:
-                    break;
-                default:
-                    break;
-            }
+            append(pos, outBuf, bufSize, tokenOps[i].literal, tokenOps[i].literal_len);
+            const int handerPos = static_cast<const int>(tokenOps[i].type);
+            tokHanlders[handerPos](pos, outBuf, bufSize, msgType, file, func, line, str, temp,
+                                   LOGGER_MAX_TEMP_SIZE);
         }
 
         if (pos < bufSize) {
@@ -324,10 +274,176 @@ private:
         size_t literal_len;
     };
 
+    /**
+     * @brief append
+     * @param pos position to place sting in outBuf
+     * @param outBuf buffer to place string
+     * @param bufSize size of buffer
+     * @param data string to place in buffer
+     * @param dataLen string length
+     *
+     * Places string in buffer in needed position
+     */
+    static void append(
+        size_t &pos, char *outBuf, size_t bufSize, const char *data, size_t dataLen) {
+        size_t cur_pos = pos;
+        size_t cnt = 0;
+
+        if (cur_pos + dataLen < bufSize) {
+            cnt = dataLen;
+        } else if (bufSize > cur_pos) {
+            cnt = bufSize - cur_pos - 1;
+        } else {
+            cnt = 0;
+        }
+        if (cnt != 0) {
+            std::memcpy(outBuf + cur_pos, data, cnt);
+            cur_pos += cnt;
+        }
+        pos = cur_pos;
+    }
+
+    static void tokDateHandler(size_t &pos,
+                               char *outBuf,
+                               size_t bufSize,
+                               [[maybe_unused]] const Log::messageType msgType,
+                               [[maybe_unused]] const char *file,
+                               [[maybe_unused]] const char *func,
+                               [[maybe_unused]] int line,
+                               [[maybe_unused]] const char *str,
+                               char *temp,
+                               size_t temp_size) {
+        data_provider->getCurrentDate(temp, temp_size);
+        append(pos, outBuf, bufSize, temp, std::strlen(temp));
+    }
+
+    static void tokTimeHandler(size_t &pos,
+                               char *outBuf,
+                               size_t bufSize,
+                               [[maybe_unused]] const Log::messageType msgType,
+                               [[maybe_unused]] const char *file,
+                               [[maybe_unused]] const char *func,
+                               [[maybe_unused]] int line,
+                               [[maybe_unused]] const char *str,
+                               char *temp,
+                               size_t temp_size) {
+        data_provider->getCurrentTime(temp, temp_size);
+        append(pos, outBuf, bufSize, temp, std::strlen(temp));
+    }
+
+    static void tokLevelHandler(size_t &pos,
+                                char *outBuf,
+                                size_t bufSize,
+                                [[maybe_unused]] const Log::messageType msgType,
+                                [[maybe_unused]] const char *file,
+                                [[maybe_unused]] const char *func,
+                                [[maybe_unused]] int line,
+                                [[maybe_unused]] const char *str,
+                                [[maybe_unused]] char *temp,
+                                [[maybe_unused]] size_t temp_size) {
+        const char *ch = msg_log_types[static_cast<const int>(msgType)];
+        append(pos, outBuf, bufSize, ch, std::strlen(ch));
+    }
+
+    static void tokFileHandler(size_t &pos,
+                               char *outBuf,
+                               size_t bufSize,
+                               [[maybe_unused]] const Log::messageType msgType,
+                               [[maybe_unused]] const char *file,
+                               [[maybe_unused]] const char *func,
+                               [[maybe_unused]] int line,
+                               [[maybe_unused]] const char *str,
+                               [[maybe_unused]] char *temp,
+                               [[maybe_unused]] size_t temp_size) {
+        append(pos, outBuf, bufSize, file, std::strlen(file));
+    }
+
+    static void tokThreadHandler(size_t &pos,
+                                 char *outBuf,
+                                 size_t bufSize,
+                                 [[maybe_unused]] const Log::messageType msgType,
+                                 [[maybe_unused]] const char *file,
+                                 [[maybe_unused]] const char *func,
+                                 [[maybe_unused]] int line,
+                                 [[maybe_unused]] const char *str,
+                                 char *temp,
+                                 size_t temp_size) {
+        data_provider->getThreadId(temp, temp_size);
+        append(pos, outBuf, bufSize, temp, std::strlen(temp));
+    }
+
+    static void tokFuncHandler(size_t &pos,
+                               char *outBuf,
+                               size_t bufSize,
+                               [[maybe_unused]] const Log::messageType msgType,
+                               [[maybe_unused]] const char *file,
+                               [[maybe_unused]] const char *func,
+                               [[maybe_unused]] int line,
+                               [[maybe_unused]] const char *str,
+                               [[maybe_unused]] char *temp,
+                               [[maybe_unused]] size_t temp_size) {
+        append(pos, outBuf, bufSize, func, std::strlen(func));
+    }
+
+    static void tokLineHandler(size_t &pos,
+                               char *outBuf,
+                               size_t bufSize,
+                               [[maybe_unused]] const Log::messageType msgType,
+                               [[maybe_unused]] const char *file,
+                               [[maybe_unused]] const char *func,
+                               [[maybe_unused]] int line,
+                               [[maybe_unused]] const char *str,
+                               [[maybe_unused]] char *temp,
+                               [[maybe_unused]] size_t temp_size) {
+        char numbuf[LOGGER_MAX_NUMBUF_SIZE];
+        int n = std::snprintf(numbuf, sizeof(numbuf), "%d", line);
+        append(pos, outBuf, bufSize, numbuf, static_cast<size_t>(n));
+    }
+
+    static void tokPidHandler(size_t &pos,
+                              char *outBuf,
+                              size_t bufSize,
+                              [[maybe_unused]] const Log::messageType msgType,
+                              [[maybe_unused]] const char *file,
+                              [[maybe_unused]] const char *func,
+                              [[maybe_unused]] int line,
+                              [[maybe_unused]] const char *str,
+                              char *temp,
+                              size_t temp_size) {
+        data_provider->getProcessName(temp, temp_size);
+        append(pos, outBuf, bufSize, temp, std::strlen(temp));
+    }
+
+    static void tokMessageHandler(size_t &pos,
+                                  char *outBuf,
+                                  size_t bufSize,
+                                  [[maybe_unused]] const Log::messageType msgType,
+                                  [[maybe_unused]] const char *file,
+                                  [[maybe_unused]] const char *func,
+                                  [[maybe_unused]] int line,
+                                  [[maybe_unused]] const char *str,
+                                  [[maybe_unused]] char *temp,
+                                  [[maybe_unused]] size_t temp_size) {
+        append(pos, outBuf, bufSize, str, std::strlen(str));
+    }
+
+    static void tokInvalidHandler([[maybe_unused]] size_t &pos,
+                                  [[maybe_unused]] char *outBuf,
+                                  [[maybe_unused]] size_t bufSize,
+                                  [[maybe_unused]] const Log::messageType msgType,
+                                  [[maybe_unused]] const char *file,
+                                  [[maybe_unused]] const char *func,
+                                  [[maybe_unused]] int line,
+                                  [[maybe_unused]] const char *str,
+                                  [[maybe_unused]] char *temp,
+                                  [[maybe_unused]] size_t temp_size) {}
+
     static int log_level;
     static ILogSink *sinks[LOGGER_MAX_SINKS];
     static int sink_count;
+    /// class that provides platform-dependent data
     static DataProvider *data_provider;
+    /// callback to print logging message
     static void (*user_handler)(const messageType &msgType, const char *message, size_t msg_size);
 
     /// holds pointers to tokens, so the output will look the  same as @brief setMessagePattern
@@ -337,14 +453,30 @@ private:
     /// holds messages that placed after tokens passed in  @brief setMessagePatter
     static char literal_buffer[LOGGER_LITERAL_BUFFER_SIZE];
 
+    /// types of logging level, added to output message
+    static constexpr const char *msg_log_types[] = {"FATAL", "ERROR", "WARN", "INFO", "DEBUG"};
+
     /// tokens for message pattern
     static constexpr const char *tokens[] = {"%{date}", "%{time}",   "%{type}",
                                              "%{file}", "%{thread}", "%{function}",
                                              "%{line}", "%{pid}",    "%{message}"};
     static constexpr size_t num_token_types = sizeof(tokens) / sizeof(tokens[0]);
 
-    /// types of logging level, added to output message
-    static constexpr const char *msg_log_types[] = {"FATAL", "ERROR", "WARN", "INFO", "DEBUG"};
+    using TokHandlerFunc = void (*)(size_t &pos,
+                                    char *outBuf,
+                                    size_t bufSize,
+                                    const Log::messageType msgType,
+                                    const char *file,
+                                    const char *func,
+                                    int line,
+                                    const char *str,
+                                    char *temp,
+                                    size_t temp_size);
+    /// Handlers to print data in logging message, handlers should be in the same place as
+    /// corresponding tokens in array `tokens`
+    static constexpr TokHandlerFunc tokHanlders[] = {
+        tokDateHandler, tokTimeHandler, tokLevelHandler, tokFileHandler,   tokThreadHandler,
+        tokFuncHandler, tokLineHandler, tokPidHandler,   tokMessageHandler};
 };
 
 }  // namespace Log
